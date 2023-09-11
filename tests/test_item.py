eca7f9a88589cdacb4a565c3324a11b96f87f657
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-11 15:09:18 +0300

    Added test_item_init function in test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def item_fixture():
+    return Item('Смартфон Samsung', 40000, 2)
+
+
+def test_item_init(item_fixture):
+    assert item_fixture.name == 'Смартфон Samsung'
+    assert item_fixture.price == 40000
+    assert item_fixture.quantity == 2
+    assert len(Item.all) == 1
