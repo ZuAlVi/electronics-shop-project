4f9e0f3f3e3cc0a44eff9c8cb53380e70fca8e06
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-27 15:22:11 +0300

    Fixed description of the __init__ method in the Keyboard class

--- src/keyboard.py
+++ src/keyboard.py
@@ -16,7 +16,7 @@ class Keyboard(Item, SwitchLanguageMixin):
     def __init__(self, name: str, price: float, quantity: int):
         """Создание экземпляра класса Keyboard.
 
-        :param name, price � quantit подтягивается через функцию super из класса Item.
+        :param name, price и quantity подтягивается через функцию super из класса Item.
         :protected param language - язык раскладки клавиатуры(по умолчанию 'EN')."""
         super().__init__(name, price, quantity)
         self._language = 'EN'
