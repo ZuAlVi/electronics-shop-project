d31cb3da82772c9e7f19e6b2ddbb4033943eab90
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-27 13:43:19 +0300

    Added test_keyboard_init function in test_keyboard.py

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -1,3 +1,17 @@
+import pytest
 
+from src.keyboard import Keyboard
+
+
+@pytest.fixture
+def keyboard_fixture():
+    return Keyboard('Dark Project KD87A', 9600, 5)
+
+
+def test_keyboard_init(keyboard_fixture):
+    assert keyboard_fixture.name == 'Dark Project KD87A'
+    assert keyboard_fixture.price == 9600
+    assert keyboard_fixture.quantity == 5
+    assert keyboard_fixture.language == 'EN'
 
 
