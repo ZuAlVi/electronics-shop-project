5e5d6c5c67dc3c55497cf383494c857e4539a203
Alexandr Zubarev <zualvi@yandex.ru>
2023-10-02 13:38:45 +0300

    Added __init__ method body in InstantiateCSVError class

--- src/errors.py
+++ src/errors.py
@@ -1,3 +1,8 @@
 class InstantiateCSVError(Exception):
-    pass
+    def __init__(self, *args):
+        """Файл item.csv поврежден"""
+        if args:
+            self.message = args[0]
+        else:
+            self.message = 'InstantiateCSVError: Файл item.csv поврежден'
 
